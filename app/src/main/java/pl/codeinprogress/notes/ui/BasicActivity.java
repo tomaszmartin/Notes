e5ff924f2915f9241a0985d3deedee40b418c13c
tomaszmartin <tomasz.martin@gmail.com>
2016-06-05 23:11:23 +0200

    Updated for working with firebase

--- app/src/main/java/pl/codeinprogress/notes/ui/BasicActivity.java
+++ app/src/main/java/pl/codeinprogress/notes/ui/BasicActivity.java
@@ -1,9 +1,15 @@
 package pl.codeinprogress.notes.ui;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.util.Log;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
 /**
  * Created by tomaszmartin on 02.07.15.
@@ -38,5 +44,30 @@ public class BasicActivity extends AppCompatActivity {
             recreate();
         }
     }
+
+    public FirebaseStorage getStorage() {
+        if (getApplication() instanceof BasicApplication) {
+            return ((BasicApplication) getApplication()).getStorage();
+        }
+        return null;
+    }
+
+    public FirebaseDatabase getDatabase() {
+        if (getApplication() instanceof BasicApplication) {
+            return ((BasicApplication) getApplication()).getDatabase();
+        }
+        return null;    }
+
+    public FirebaseAuth getAuth() {
+        if (getApplication() instanceof BasicApplication) {
+            return ((BasicApplication) getApplication()).getAuth();
+        }
+        return null;    }
+
+    public FirebaseAnalytics getAnalytics() {
+        if (getApplication() instanceof BasicApplication) {
+            return ((BasicApplication) getApplication()).getAnalytics();
+        }
+        return null;    }
 
 }
--- app/src/main/java/pl/codeinprogress/notes/ui/BasicApplication.java
+++ app/src/main/java/pl/codeinprogress/notes/ui/BasicApplication.java
@@ -3,6 +3,9 @@ package pl.codeinprogress.notes.ui;
 import android.app.Application;
 import android.support.v7.app.AppCompatDelegate;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 
 /**
  * Created by tomaszmartin on 02.06.2016.
@@ -11,12 +14,36 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 public class BasicApplication extends Application {
 
     private FirebaseAnalytics analytics;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FirebaseDatabase getDatabase() {
+        return database;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public FirebaseAnalytics getAnalytics() {
+        return analytics;
+    }
+
+    private FirebaseStorage storage;
 
     @Override
     public void onCreate() {
         super.onCreate();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
         analytics = FirebaseAnalytics.getInstance(this);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        database.setPersistenceEnabled(true);
+        storage = FirebaseStorage.getInstance();
     }
 
 }
