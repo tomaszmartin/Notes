1c0a889238df2e9543cf0011bca97ee16cd355b5
tomaszmartin <tomasz.martin@gmail.com>
2016-06-26 20:26:33 +0200

    Updated MainActivity to use DataBinding

--- app/src/main/java/pl/codeinprogress/notes/ui/MainActivity.java
+++ app/src/main/java/pl/codeinprogress/notes/ui/MainActivity.java
@@ -1,32 +1,22 @@
 package pl.codeinprogress.notes.ui;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.AbsListView;
-import android.widget.ImageButton;
-import android.widget.ListView;
-
 import com.google.firebase.database.DatabaseReference;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import pl.codeinprogress.notes.R;
 import pl.codeinprogress.notes.adapters.NotesAdapter;
 import pl.codeinprogress.notes.data.NotesProvider;
-import pl.codeinprogress.notes.model.Note;
+import pl.codeinprogress.notes.databinding.MainActivityBinding;
 import pl.codeinprogress.notes.firebase.FirebaseActivity;
 import pl.codeinprogress.notes.firebase.FirebaseLink;
+import pl.codeinprogress.notes.model.Note;
 import pl.codeinprogress.notes.ui.listeners.NavigationListener;
 import pl.codeinprogress.notes.ui.listeners.NotesListener;
 import pl.codeinprogress.notes.ui.listeners.OnAddListener;
@@ -34,23 +24,16 @@ import pl.codeinprogress.notes.ui.listeners.OnSelectListener;
 
 public class MainActivity extends FirebaseActivity implements OnSelectListener, OnAddListener {
 
-    @Bind(R.id.navigationView) NavigationView navigationView;
-    @Bind(R.id.toolbar) Toolbar toolbar;
-    @Bind(R.id.drawerLayout) DrawerLayout drawerLayout;
-    @Bind(R.id.fab) FloatingActionButton fab;
-    @Bind(R.id.listView) ListView listView;
-    @Bind(R.id.emptyList) View emptyList;
-    @Bind(R.id.clearSearchutton) ImageButton clearSearchutton;
     private NotesListener notesListener;
     private NotesAdapter adapter;
     private MenuItem searchItem;
     private NotesProvider provider;
+    private MainActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        ButterKnife.bind(this);
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
 
         setupView();
         setupData();
@@ -95,7 +78,7 @@ public class MainActivity extends FirebaseActivity implements OnSelectListener, 
         } else if (id == R.id.action_sort_newest) {
             return true;
         } else if (id == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
+            binding.drawerLayout.openDrawer(GravityCompat.START);
         } else if (id == R.id.action_select_all) {
 
         } else if (id == R.id.action_night_mode) {
@@ -122,29 +105,29 @@ public class MainActivity extends FirebaseActivity implements OnSelectListener, 
     }
 
     private void setupView() {
-        setSupportActionBar(toolbar);
+        setSupportActionBar(binding.toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+            binding.toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
         }
     }
 
     private void setupData() {
         provider = new NotesProvider(this);
         DatabaseReference notesReference = getDatabase().getReference(FirebaseLink.forNotes());
-        NavigationListener navigationListener = new NavigationListener(this, drawerLayout);
+        NavigationListener navigationListener = new NavigationListener(this, binding.drawerLayout);
 
         adapter = new NotesAdapter(this, Note.class, R.layout.main_item, notesReference);
         notesListener = new NotesListener(this, adapter);
 
-        listView.setAdapter(adapter);
-        listView.setEmptyView(emptyList);
-        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
-        navigationView.setNavigationItemSelectedListener(navigationListener);
-        listView.setMultiChoiceModeListener(notesListener);
-        listView.setOnItemClickListener(notesListener);
-        fab.setOnClickListener(notesListener);
-        clearSearchutton.setOnClickListener(null);
+        binding.listView.setAdapter(adapter);
+        binding.listView.setEmptyView(binding.emptyList);
+        binding.listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        binding.navigationView.setNavigationItemSelectedListener(navigationListener);
+        binding.listView.setMultiChoiceModeListener(notesListener);
+        binding.listView.setOnItemClickListener(notesListener);
+        binding.fab.setOnClickListener(notesListener);
+        binding.clearSearchutton.setOnClickListener(null);
     }
 
 }
