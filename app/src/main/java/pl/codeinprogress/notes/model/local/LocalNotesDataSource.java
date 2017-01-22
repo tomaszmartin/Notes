fbef96154f4b4a4fa429a04fa1cde78f1dc91f9d
tomaszmartin <tomasz.martin@gmail.com>
2017-01-22 23:01:15 +0100

    Updated LocalNotesDataSource

--- app/src/main/java/pl/codeinprogress/notes/model/local/LocalNotesDataSource.java
+++ app/src/main/java/pl/codeinprogress/notes/model/local/LocalNotesDataSource.java
@@ -2,17 +2,23 @@ package pl.codeinprogress.notes.model.local;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.annotation.BoolRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.squareup.sqlbrite.BriteDatabase;
 import com.squareup.sqlbrite.SqlBrite;
 
+import java.util.List;
+
+import io.reactivex.Observable;
 import pl.codeinprogress.notes.model.Note;
+import pl.codeinprogress.notes.model.NotesDataSource;
 import pl.codeinprogress.notes.util.SchedulerProvider;
 import rx.functions.Func1;
+import static pl.codeinprogress.notes.model.local.NotesContract.*;
 
-public class LocalNotesDataSource {
+public class LocalNotesDataSource implements NotesDataSource {
 
     @Nullable
     private static LocalNotesDataSource instance;
@@ -25,6 +31,50 @@ public class LocalNotesDataSource {
         LocalNotesDatabaseHelper databaseHelper = new LocalNotesDatabaseHelper(context);
         SqlBrite sqlBrite = new SqlBrite.Builder().build();
         database = sqlBrite.wrapDatabaseHelper(databaseHelper, schedulerProvider.io());
+    }
+
+    public static LocalNotesDataSource getInstance(@NonNull Context context, @NonNull SchedulerProvider schedulerProvider) {
+        if (instance == null) {
+            instance = new LocalNotesDataSource(context, schedulerProvider);
+        }
+
+        return instance;
+    }
+
+    @Override
+    public Observable<List<Note>> getNotes() {
+        return null;
+    }
+
+    @Override
+    public Observable<Note> getNote(@NonNull String noteId) {
+        return null;
+    }
+
+    @Override
+    public void saveNote(@NonNull Note note) {
+
+    }
+
+    @Override
+    public void deleteTask(@NonNull Note note) {
+
+    }
+
+
+
+    @NonNull
+    private Note getNote(@NonNull Cursor cursor) {
+        Note note = new Note();
+        note.setId(cursor.getString(cursor.getColumnIndexOrThrow(ENTRY_ID)));
+        note.setTitle(cursor.getString(cursor.getColumnIndexOrThrow(TITLE)));
+        note.setDescription(cursor.getString(cursor.getColumnIndexOrThrow(DESCRIPTION)));
+        note.setPath(cursor.getString(cursor.getColumnIndexOrThrow(PATH)));
+        note.setCreated(cursor.getLong(cursor.getColumnIndexOrThrow(CREATED)));
+        note.setModified(cursor.getLong(cursor.getColumnIndexOrThrow(MODIFIED)));
+        note.setSecured(cursor.getInt(cursor.getColumnIndexOrThrow(SECURED)) == 1);
+
+        return note;
     }
 
 }
