4c896a88237fe0d372d7c26560a3f5679f81da71
tomaszmartin <tomasz.martin@gmail.com>
2017-02-04 12:59:56 +0100

    Added dictating notes functionality

--- app/src/main/java/pl/codeinprogress/notes/view/DetailsActivity.java
+++ app/src/main/java/pl/codeinprogress/notes/view/DetailsActivity.java
@@ -1,11 +1,13 @@
 package pl.codeinprogress.notes.view;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
@@ -14,6 +16,7 @@ import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.Date;
@@ -63,6 +66,8 @@ public class DetailsActivity extends BaseActivity implements DetailsView {
                     presenter.transformImage(data.getData(), this);
                     break;
                 case AUDIO_REQUEST_CODE:
+                    String text = data.getDataString();
+                    insertText(text);
                     break;
                 default:
                     break;
@@ -126,6 +131,26 @@ public class DetailsActivity extends BaseActivity implements DetailsView {
 
 
 
+
+
+    private void insertText(String txt) {
+        EditText editor = (EditText) findViewById(R.id.editor);
+        editor.setText(getNoteContent() + "\n" + txt);
+    }
+
+    private void dictate() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speak_now));
+        try {
+            startActivityForResult(intent, AUDIO_REQUEST_CODE);
+        } catch (ActivityNotFoundException a) {
+            showErrorMessage(getString(R.string.dictating_not_supported));
+        }
+    }
 
     private void setupView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
